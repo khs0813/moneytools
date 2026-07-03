9a9c056dd678723d96b4c642153ccea63049e5b8
hyeonseop <loveremain67@gmail.com>
2026-07-03 19:14:10 +0900

    coupang remove

--- src/main/java/com/example/moneytools/controller/GlobalModelAdvice.java
+++ src/main/java/com/example/moneytools/controller/GlobalModelAdvice.java
@@ -56,8 +56,8 @@ public class GlobalModelAdvice {
         return SitePages.sitemap().stream()
                 .map(page -> page.lastModified().format(DateTimeFormatter.BASIC_ISO_DATE))
                 .max(String::compareTo)
-                .map(v -> v + "-moneycomma-v6")
-                .orElse("20260425-moneycomma-v6");
+                .map(v -> v + "-moneycomma-v8")
+                .orElse("20260425-moneycomma-v8");
     }
 
     @ModelAttribute("defaultImageUrl")
